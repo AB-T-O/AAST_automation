b9fd440c284e39652c00d2c8a2e64355dc1dfc62
Omit <abdalrhmntarqthmant132004@gmail.com>
2025-02-08 00:43:12 +0200

    69_420

--- main.py
+++ main.py
@@ -8,30 +8,26 @@ def login_s1(ID, pincod):
      driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_UserName\"]").send_keys(ID)
      driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_Password\"]").send_keys(pincod)
      driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_btn_login\"]").click()
-
-
 def s2():
     driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_lmajor\"]").click()
-
 def s3():
     driver.find_element(By.XPATH,"// *[ @ id = \"ctl00_ContentPlaceHolder1_Lbtn_Reg\"]").click()
+def s4(what_to_select):
+     dropdown_menu = driver.find_element(By.XPATH,"/html/body/main/div/form/div/div[2]/label[1]/select")
+     dropdown_element =Select(dropdown_menu)
+     ops = dropdown_element.options
+     ops_text =[]
+     for op in ops:
+        ops_text.append(op.text.lower())
+     for text in ops_text:
+         if what_to_select.lower() in text:
+            dropdown_element.select_by_index(ops_text.index(text))
+            break
+
 
 login_s1(231014333,151431)
 s2()
 s3()
-###dropdown_menu = driver.find_element(By.XPATH,"/html/body/main/div/form/div/div[2]/label[1]/select")
-#dropdown_element =Select(dropdown_menu)
-#ops = dropdown_element.options
-#ops_text =[]
-#what_to_select = "two"
-#for op in ops:  # note to self this is bad code do not  after demo
-    #ops_text.append(op.text.lower())
-
-#for text in ops_text:  # note to self this is bad code do not  after demo
-    #if what_to_select.lower() in text:
-        #dropdown_element.select_by_index(ops_text.index(text))
-        #break
+#s4()
 
 
-#button.click()
-###ctl00_ContentPlaceHolder1_UserName
