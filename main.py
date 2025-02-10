9b245b1f487a8b4adf4966e533cfbe389d524798
AB-T-O <abdalrhmntarqthmant132004@gmail.com>
2025-02-11 00:02:01 +0200

    all good 420_69

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+id=int(input("Enter ID: "))
+pid=input("Enter PIN: ")
+cs=input("Enter cs: ")
 driver = webdriver.Firefox()
 driver.get("https://alexreg.aast.edu/aastreg/frm_login.aspx")#https://www.selenium.dev/selenium/web/web-form.html
 def login_s1(ID, pincod):
@@ -19,7 +22,7 @@ def s3():
     driver.find_element(By.XPATH,"// *[ @ id = \"ctl00_ContentPlaceHolder1_Lbtn_Reg\"]").click()
 
 def s4():
-    driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_lbtn_InsertTermCourse\"]").click()
+    driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_lbtn_changeReg\"]").click()
 
 def fun(xp,xpaths,what_to_select):
     try:
@@ -44,9 +47,8 @@ def s5(what_to_select):
          while True:
              if fun(xp,xpaths,what_to_select):
                  break
-
-login_s1(231014333,151431)#enter your di and pin cod
+login_s1(id,pid)#enter your di and pin cod
 s2()
 s3()
-s4() #do not run this if you whan to register for the 1st time
-s5("4cs7")
+s4() 
+s5(cs)
