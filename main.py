e341369bac6b16e54a7f76e910c6a887f8b33005
Omit <abdalrhmntarqthmant132004@gmail.com>
2025-02-10 23:39:11 +0200

    good to go

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import wait
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://alexreg.aast.edu/aastreg/frm_login.aspx")#https://www.selenium.dev/selenium/web/web-form.html
@@ -16,30 +18,35 @@ def s2():
 def s3():
     driver.find_element(By.XPATH,"// *[ @ id = \"ctl00_ContentPlaceHolder1_Lbtn_Reg\"]").click()
 
-def s_temp():
-    driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_lbtn_changeReg\"]").click()
+def s4():
+    driver.find_element(By.XPATH,"//*[@id=\"ctl00_ContentPlaceHolder1_lbtn_InsertTermCourse\"]").click()
 
+def fun(xp,xpaths,what_to_select):
+    try:
+        dropdown_menu = driver.find_element(By.XPATH,xp)
+        dropdown_element =Select(dropdown_menu)
+        ops = dropdown_element.options
+        ops_text =[]
+        for op in ops:
+            ops_text.append(op.text.lower())
+        for text in ops_text:
+            if what_to_select.lower() in text:
+                dropdown_element.select_by_index(ops_text.index(text))
+                break
+        ##time.sleep(2)
+        return True
+    except:
+        return False
 
 def s5(what_to_select):
     xpaths = {"//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl02_drp_cls\"]","//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl03_drp_cls\"]","//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl04_drp_cls\"]","//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl05_drp_cls\"]","//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl06_drp_cls\"]","//*[@id=\"ctl00_ContentPlaceHolder1_grdvw_courses_ctl07_drp_cls\"]"}
     for xp in xpaths :
-         dropdown_menu = driver.find_element(By.XPATH,xp)
-         dropdown_element =Select(dropdown_menu)
-         ops = dropdown_element.options
-         ops_text =[]
-         for op in ops:
-            ops_text.append(op.text.lower())
-         for text in ops_text:
-             if what_to_select.lower() in text:
-                dropdown_element.select_by_index(ops_text.index(text))
-                break
-         time.sleep(2)
+         while True:
+             if fun(xp,xpaths,what_to_select):
+                 break
 
 login_s1(231014333,151431)#enter your di and pin cod
 s2()
 s3()
-s_temp()
-time.sleep(2)
+s4() #do not run this if you whan to register for the 1st time
 s5("4cs7")
-
-
